a14894a30119e7c942f7c67dc283d82bc3185158
Yixun Liang <lyxun2000@163.com>
2025-11-20 13:26:41 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from diffusionGS.pipline_obj import DiffusionGSPipeline
 import torch
 from torchvision.utils import save_image
-## export U2NET_HOME=/mnt/bn/yixun3d/u2net_cache
+
 pipeline = DiffusionGSPipeline.from_pretrained("CaiYuanhao/DiffusionGS", device="cuda:0", torch_dtype=torch.float16)
 gs_output = pipeline("extra_files/test_cases/dp.png",seed=62, foreground_ratio = 0.825, extract_mesh=True)#for the case that with pose
 ##export gaussians
